c0bf5b09e9a1c7533eab31e528d7bc58f56579cb
thoma1c <44304301+thoma1c@users.noreply.github.com>
2023-11-30 20:31:24 +0000

    Changes as of Nov 30

---
+++ scripts/test.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+"""test.py: Testing running python files in this crazy codepace."""
+
+__author__      = "Tyler Thomas"
+__copyright__   = "Copyright 2023, Planet Earth"
+
+def myFunc(word):
+	return word
+
+word = "I guess this works!"
+
+print(myFunc(word))
